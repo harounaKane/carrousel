c7f860953a9f2c23b8ad10fb71fbe3038f0402e6
harounaKane <harouna.Kane@hotmail.fr>
2025-06-11 15:16:19 +0200

    creation des evenements boutons en js

--- js/script.js
+++ js/script.js
@@ -1,2 +1,38 @@
 
-console.log("test");
+let menu = document.getElementById("toolbar-toggle");
+let toolbar =  document.getElementsByClassName("toolbar")[0];
+let sliderPrevious = document.getElementById("slider-previous");
+let sliderToggle = document.getElementById("slider-toggle");
+let sliderNext = document.getElementById("slider-next");
+let sliderRandom= document.getElementById("slider-random");
+
+sliderPrevious.addEventListener("click", previous);
+sliderToggle.addEventListener("click", play);
+sliderNext.addEventListener("click", next);
+sliderRandom.addEventListener("click", random);
+
+
+menu.addEventListener("click", () => {
+    barreOutil();
+});
+
+function previous(){
+    console.log("previous");
+}
+
+function play(){
+    console.log("play");
+}
+
+function next(){
+    console.log("next");
+}
+
+function random(){
+    console.log("random");
+}
+
+function barreOutil(){
+    toolbar.classList.toggle("d-none");
+}
+
